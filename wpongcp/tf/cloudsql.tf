74217ce900b7658169706d2c9c61f0815d423cf6
robert <X00204385@myTUDublin.ie>
2023-05-29 10:35:08 +0100

    Have CloudSQL working from private IP. Was able to connect to this and setup wordpress.

--- wpongcp/tf/cloudsql.tf
+++ wpongcp/tf/cloudsql.tf
@@ -1,61 +1,59 @@
-# # Create a Cloud SQL instance and enable access to it from the VM instances
-# # created. 
-# #
-# # https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/sql_database_instance
-# # https://cloud.google.com/sql/docs/mysql/authorize-networks
+# Create a Cloud SQL instance and enable access to it from the VM instances
+# created. 
+#
+# https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/sql_database_instance
+# https://cloud.google.com/sql/docs/mysql/authorize-networks
 
-# # create Cloud SQL instance
-# resource "google_sql_database_instance" "wordpress" {
-#   name             = "wordpress-instance-${var.suffix}"
-#   region           = var.gcp_region_1
-#   database_version = "MYSQL_5_7"
+# create Cloud SQL instance
+resource "google_sql_database_instance" "wordpress" {
+  name             = "wordpress-instance-${var.suffix}"
+  region           = var.gcp_region_1
+  database_version = "MYSQL_5_7"
+  provider         = google-beta
 
-#   deletion_protection = false
+  deletion_protection = false
 
+  settings {
+    tier = "db-f1-micro"
 
-#   settings {
-#     tier = "db-f1-micro"
+    disk_size         = 10
+    disk_autoresize   = false
+    disk_type         = "PD_HDD"
+    availability_type = "ZONAL"
 
-#       disk_size = 10
-#       disk_autoresize = false
-#       disk_type = "PD_HDD"
-#       availability_type = "ZONAL"
+    backup_configuration {
+      enabled = false
+    }
 
-#     ip_configuration {
+    ip_configuration {
 
-#       # # Home network connection (debugging)
-#       # authorized_networks {
-#       #   name = "home laptop"
-#       #   value = "1.2.3.4/32"
-#       # }
+      ipv4_enabled = false
 
-#       dynamic "authorized_networks" {
-#         for_each = google_compute_instance.apps
-#         iterator = apps
+      enable_private_path_for_google_cloud_services = true
 
-#         content {
-#           name  = apps.value.name
-#           value = apps.value.network_interface.0.access_config.0.nat_ip
-#         }
-#       }
-#     }
-#     backup_configuration {
-#       enabled = false
-#     }
-#   }
-# }
+      private_network = data.google_compute_network.vpc.id
 
-# # create Cloud SQL user
-# resource "google_sql_user" "wordpress_user" {
-#   name     = "wp_user"
-#   instance = google_sql_database_instance.wordpress.name
-#   host     = "%"
-#   password = "Computing1"
-# }
+      # # Home network connection (debugging)
+      # authorized_networks {
+      #   name = "home laptop"
+      #   value = "1.2.3.4/32"
+      # }
 
-# # create Cloud SQL database
-# resource "google_sql_database" "wp" {
-#   name     = "wp"
-#   instance = google_sql_database_instance.wordpress.name
-# }
+    }
+  }
+}
+
+# Create MySQL user
+resource "google_sql_user" "wordpress_user" {
+  name     = "wp_user"
+  instance = google_sql_database_instance.wordpress.name
+  host     = "%"
+  password = "Computing1"
+}
+
+# # create MySQL database
+resource "google_sql_database" "wp" {
+  name     = "wp"
+  instance = google_sql_database_instance.wordpress.name
+}
 
